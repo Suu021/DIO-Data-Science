7934d38d785f4a40eb607c245f5d49a2b49465f9
Suu Kirinus Nogueira <50893598+Suu021@users.noreply.github.com>
2022-11-03 09:38:48 -0300

    terminei o package

---
+++ image-processing-package/image_processing/processing/combination.py
@@ -0,0 +1,15 @@
+import numpy as np
+from skimage.color import rgb2gray
+from skimage.exposure import match_histograms
+from skimage.metrics import structural_similarity
+
+def find_difference(image1, image2):
+    assert image1.shape == image2.shape, "Specify 2 images with the same shape."
+    gray_image1 = rgb2gray(image1)
+    gray_image2 = rgb2gray(image2)
+    (score, difference_image) = structural_similarity(gray_image1, gray_image2, full=True)
+    print("Similarity of the images: ", score)
+    return (difference_image-np.min(difference_image))/(np.max(difference_image)-np.min(difference_image))
+
+def transfer_histogram(image1, image2):
+    return match_histograms(image1, image2, multichannel=True)
--- image-processing-package/package_name/processing/combination.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-from skimage.color import rgb2gray
-from skimage.exposure import match_histograms
-from skimage.metrics import structural_similarity
